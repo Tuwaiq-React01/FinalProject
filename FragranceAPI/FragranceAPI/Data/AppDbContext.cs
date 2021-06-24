74980dedaede4b0449f37abd811e5edc567da53e
Arwa wanla <arwawanla@Arwas-MacBook-Pro-4.local>
2021-06-24 08:34:20 +0300

    API

---
+++ FragranceAPI/FragranceAPI/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using FragranceStoreAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FragranceStoreAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Fragrance> fragrances { get; set; }
+        // public DbSet<FragranceReviews> Reviews { get; set; }
+    }
+}
