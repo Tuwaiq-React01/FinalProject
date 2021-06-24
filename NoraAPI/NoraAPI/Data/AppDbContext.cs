c3362586fad481cbfa1b1506f6f547a5215858a7
Norah <Norahmutairi@outlook.sa>
2021-06-24 08:35:15 +0300

    add api

---
+++ NoraAPI/NoraAPI/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using GreenLifeStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoraAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ProductModel> Products { get; set; }
+
+        /*
+        //insert Products
+        modelBuilder.Entity<ProductModel>().HasData(new Product{ ProductId = 1, Name = "Apple", Price = 1.5f, Image = "https://www.othaimmarkets.com/media/catalog/product/cache/4/small_image/170x165/9df78eab33525d08d6e5fb8d27136e95/1/3/132.jpg" });
+        modelBuilder.Entity<ProductModel>().HasData(new Product{ ProductId = 2, Name = "Orange", Price = 1.5f, Image = "https://www.othaimmarkets.com/media/catalog/product/cache/4/small_image/340x330/9df78eab33525d08d6e5fb8d27136e95/o/r/orange-2.jpg" });
+
+        modelBuilder.Entity<ProductModel>().HasData(new Product { ProductId = 3, Name = "Banana", Price = 3.5f, Image = "https://www.othaimmarkets.com/media/catalog/product/cache/4/small_image/340x330/9df78eab33525d08d6e5fb8d27136e95/5/0/500_0.jpg" });
+        */
+
+    }
+}
+
+
---
+++ NoraAPI/NoraAPI/Migrations/20210622010413_FirstMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NoraAPI.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductDetails = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
