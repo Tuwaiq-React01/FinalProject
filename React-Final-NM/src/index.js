487257f6b0ff95a61d8b42fba474292c7deba988
Nada Al-Otaibi <82446110+NadaAlOtaibi@users.noreply.github.com>
2021-06-24 15:06:52 +0300

    Update index.js

--- React-Final-NM/src/index.js
+++ React-Final-NM/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '/Users/nadaalotaibi/Downloads/React_Tutorial_Contact_Manager_App-react-crud-axios/src/components/index.css';
+import 'ndex.css';
 import ReactDOM from "react-dom";
 import { Button } from 'reactstrap';
 import App from "./components/App";
@@ -25,8 +25,6 @@ ReactDOM.render(
  <center><img className="photo" src  = "https://f.top4top.io/p_2000ydcir1.jpg" alt= ""></img></center> 
  <ul>
 
- {/* <a href="./">New Service Request</a> */}
- {/* <button class="ui button white"> <a href="./">New Service Request</a></button> */}
  <center>
  <button class="medium ui button">
   <i class="facebook icon"></i><Link to= "./Login"> Log in</Link>
@@ -43,23 +41,15 @@ ReactDOM.render(
 <button class="medium ui button">
   <i class="home icon"></i><Link to= "./"> Coffee Arts</Link>
 </button>
-{/* <button type="button"  class="medium ui button  " ><Link to= "./"><h2>Coffee lovers</h2></Link></button> */}
-{/* <button class="medium ui button"> */}
-  {/* <i class="facebook icon"></i><h3>Facebook</h3><Link to= "./Login"><h2>Log in</h2></Link></button> */}
 
 </center>
 <center><img className="p" src  = "https://l.top4top.io/p_2000eamox1.png" alt= ""></img></center> 
 
-{/* <img className="n" src="https://c.top4top.io/p_200085ysy1.jpg" alt="Cinque Terre" width="304" height="236"/>  */}
 </ul>
 
 
 <Switch>
 
-    {/* <Route  exact path= '/AddContact'>
-       <App />
-    </Route>  */}
-   
     <Route exact  path="/Login" >
       
       <Login/>
@@ -91,8 +81,3 @@ ReactDOM.render(
       document.getElementById('root')
 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
